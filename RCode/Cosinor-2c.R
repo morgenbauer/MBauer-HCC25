292069d3a4054b5c09d583cf6b1d9404643eb26e
Morgen Bauer <morgenebauer@gmail.com>
2025-06-10 10:37:13 -0500

    Adds initial R code structure
    Defines the high-level strategy for the R code development.
    Specifies goals for importing, processing, and analyzing data
    within the 2-component cosinor model framework.

--- RCode/Cosinor-2c.R
+++ RCode/Cosinor-2c.R
@@ -1,1 +1,6 @@
 #2c Cosinor
+#Purpose here is to create an R studio verson for two component cosinor model
+#Goal 1: Create path to read in GC spreadsheet
+#Goal 2: Find RStudio Libraries that know how to import .xlsx for specific tabs in them
+#Goal 3: Process imported data to remove or ignore columms E and H (HR051a)
+#Goal 4: Process data for 2c
